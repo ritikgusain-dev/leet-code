b14c6647e12966e4b5545b28e10dbca0e500e1c7
ohiRITIK <ritikgusain3311220055@gmail.com>
2026-07-19 22:57:43 +0530

    Time: 0 ms (100%), Space: 12.3 MB (47.27%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+        //we are comparing from the last bc nums1 me saare elements use nahi karne, only use first m elements , so if we start comparing from first then the elements may be over written in nums1 even before placing them in their correct place, so better compare from the last in both arrays
          int i = m - 1;        // Last valid element in nums1
         int j = n - 1;        // Last element in nums2
         int k = m + n - 1;    // Last position in nums1
@@ -18,6 +19,7 @@ public:
         }
 
         // Copy remaining elements of nums2 if any
+        //no loop for nums1 bc original changes are already being made in nums1, if nums1 ke pass kuch remaining bhi hoga compare karne ko toh vo waise bhi nums1 me apni sahi jagah par hoga since nums1 is already sorted
         while (j >= 0) {
             nums1[k] = nums2[j];
             j--;
