3c4f8a86a2694f795ac3d6ca269eec2e0d9fdac8
ohiRITIK <ritikgusain3311220055@gmail.com>
2026-08-23 14:06:49 +0530

    Time: 0 ms (100%), Space: 17.7 MB (87.38%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -16,10 +16,10 @@ public:
         int pre=1;
         int suf=1;
         for(int i=0;i<nums.size();i++){
-            if(pre==0) pre=1;
+            if(pre==0) pre=1; //if 0 is encountered reset doing the multiplication
             if(suf==0) suf=1;
             pre*=nums[i];
-            suf*=nums[n-i-1];
+            suf*=nums[n-i-1];// piche se multiplication chalta rahega
             maxx=max(maxx,max(pre,suf));
         }
         return maxx;
