74c2436a507d8ddb448b9780c2874bdcb16c84ea
ohiRITIK <ritikgusain3311220055@gmail.com>
2026-09-11 16:14:30 +0530

    Time: 52 ms (35.05%), Space: 61.8 MB (7.47%) - LeetHub

--- 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
+++ 1552-magnetic-force-between-two-balls/1552-magnetic-force-between-two-balls.cpp
@@ -1,3 +1,4 @@
+//we need to put ALL the balls, the distance between all the balls will vary, we need to find  the min distance of every possible arrangement each time and that minimum distance must be maximum possible distance of any arangement
 class Solution {
 public:
     bool canPlace(vector<int>&position,int minDis,int total){
@@ -13,8 +14,10 @@ public:
     }
     int maxDistance(vector<int>& position, int m) {
         sort(position.begin(),position.end());
-        int low=0,high=position[position.size()-1]-position[0];
+        int low=0; //minimum possible distance between 2 balls
+        int high=position[position.size()-1]-position[0]; //maximum possible distance between 2 balls
         while(low<=high){
+            //mid will be randomly picking distances between low and high and checks whether the distance is possible to utilise all the balls within the size and 
             int mid=low+(high-low)/2;
             if(canPlace(position,mid,m)) low=mid+1;
             else high=mid-1;
